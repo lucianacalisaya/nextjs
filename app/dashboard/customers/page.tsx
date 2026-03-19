a994f237f8c6e638dde4c0391bd442adfe978745
lucianacalisaya <lulacalisaya@gmail.com>
2026-03-19 20:40:52 +0100

    customers page set up

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,8 +1,38 @@
+import Pagination from '@/app/ui/invoices/pagination';
+import Table from '@/app/ui/customers/table';
+import { lusitana } from '@/app/ui/fonts';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchFilteredCustomers } from '@/app/lib/data';
 import { Metadata } from 'next';
+
  
 export const metadata: Metadata = {
   title: 'Customers',
 }; 
-export default function Page() {
-    return <p>Customers</p>
-}
+export default async function Page( {
+    searchParams,
+  } : {
+    searchParams?: {
+      query?: string;
+      page?: string;
+    };
+  }) {
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
+        
+    const customers = await fetchFilteredCustomers(query);
+    const totalPages = 1;
+
+    return (
+      <div className="w-full">
+            
+             <Suspense fallback={<InvoicesTableSkeleton />}>
+              <Table customers={customers}/>
+            </Suspense>
+            <div className="mt-5 flex w-full justify-center">
+              <Pagination totalPages={totalPages} />
+            </div>
+          </div>
+    );
+  }
--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -15,7 +15,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   const currentPage = Number(searchParams.get('page')) || 1;
  
   const createPageURL = (pageNumber: number | string) => {
-    const params = new URLSearchParams(searchParams);
+    const params = new URLSearchParams(searchParams.toString());
     params.set('page', pageNumber.toString());
     return `${pathname}?${params.toString()}`;
   };
